75d389996920d11d3da5b30bd4874e83ed6f57dd
Felipe Roberto de Oliveira Borges <felipe.borges@intranet.fapesb.ba.gov.br>
2023-06-12 10:27:22 -0300

    Iniciando estilização do APP.

---
+++ frontend/src/components/template/Logo.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Logo.css' 
+import '../../assets/images/logo.jpg'
+
+function logo() {
+  return (
+    <aside className='logo'>
+        Logo
+    </aside>
+  )
+}
+
+export default logo
