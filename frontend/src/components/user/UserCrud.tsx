ea98344b0d5af4c55091ffc4719934769997568a
Felipe Roberto de Oliveira Borges <felipe.borges@intranet.fapesb.ba.gov.br>
2023-06-30 09:53:26 -0300

    Adicionado lógica inicial do dadastro de usuário.

--- frontend/src/components/user/UserCrud.tsx
+++ frontend/src/components/user/UserCrud.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Main from "../template/Main"
+import Axios from 'axios'
 
 const headerProps = {
     icon: 'user',
@@ -7,12 +8,39 @@ const headerProps = {
     subtitle: 'Crud de usuários'
 }
 
-function UserCrud() {
-  return (
-    <Main {...headerProps}>
-        Cadastro de Usuários
-    </Main>
-  )
-}
+const baseUrl = 'http://localhost:3001/users';
+const initialState = {
+    user: {id: '',name: '', email: ''},
+    list: []
+};
+export default class UserCrud extends React.Component  {
+    state = {...initialState};
+    clear(){
+        this.setState({user: initialState.user});
+    }
 
-export default UserCrud
+    save(){
+        const user = this.state.user;
+        const method = user.id ? 'put' : 'post';
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl;
+        Axios[method](url, user).then(resp => {
+            const list = this.getUpdatedList(resp.data);
+            this.setState({user: initialState.user, list});
+        })
+    }
+
+
+    getUpdatedList(user: any){
+        const list: any = this.state.list.filter((u:any) => u.id !== user.id);
+        list.unshift(user);
+        return list;
+    }
+
+    render(){
+        return (
+            <Main {...headerProps}>
+                Cadastro de Usuários
+            </Main>
+        )
+    }
+}
